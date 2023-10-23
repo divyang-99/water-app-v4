7d0ae7c5033f457d09e36d36ea3f35e92bee0f71
Divyang Soni <dcsoni_21@mc.vjti.ac.in>
2023-10-23 16:40:20 +0530

    made minor changes in file structure

--- app.py
+++ app.py
@@ -11,8 +11,8 @@ print(os.getenv('ADMIN_PASSWORD'))  # should print 'admin@123'
 
 
 # Initialize Flask app
-app = Flask(__name__, static_folder="../static",
-            template_folder="../templates")
+app = Flask(__name__, static_folder="/static",
+            template_folder="/templates")
 app.secret_key = os.urandom(24)  # Generate a random key for session management
 
 # Prevent Caching
